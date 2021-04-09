eba93c01a25f5e74fc7c8ccb6761e0ff91b64a95
scottdomes <scottdomes@gmail.com>
2021-04-09 15:12:18 -0700

    Add more articles

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -5,7 +5,7 @@ import styles from "./Index.module.css";
 const Article = ({ post }) => {
   const { meta } = post.module;
   return (
-    <a href={`/articles${post.link}`}>
+    <a className={styles.container} href={`/articles${post.link}`}>
       <div>
         <a href={`/articles${post.link}`}>
           <h2 className={styles.title}>{meta.title}</h2>
@@ -17,9 +17,12 @@ const Article = ({ post }) => {
 };
 
 export default function IndexPage() {
+  const sortedArticles = posts.sort(
+    (a, b) => new Date(b.module.meta.date) - new Date(a.module.meta.date)
+  );
   return (
     <Layout>
-      {posts.map((post) => {
+      {sortedArticles.map((post) => {
         return <Article key={post.link} post={post} />;
       })}
     </Layout>
