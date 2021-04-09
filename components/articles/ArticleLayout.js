668375524f89db7addfc62184929b05163e737fd
scottdomes <scottdomes@gmail.com>
2021-04-09 12:53:35 -0700

    Code highlighting

--- components/articles/ArticleLayout.js
+++ components/articles/ArticleLayout.js
@@ -1,14 +1,21 @@
 import Layout from "components/layout/Layout";
 import GradientLine from "components/visual/GradientLine";
+import CodeBlock from "components/visual/CodeBlock";
+import { MDXProvider } from "@mdx-js/react";
 import styles from "./ArticleLayout.module.css";
 
 const ArticleLayout = ({ meta, children }) => {
+  const components = {
+    code: CodeBlock,
+  };
   return (
     <Layout title={meta.title}>
       <h1 className={styles.title}>{meta.title}</h1>
       <h3 className={styles.description}>{meta.description}</h3>
       <GradientLine className={styles.line} />
-      <article className={styles.content}>{children}</article>
+      <MDXProvider components={components}>
+        <article className={styles.content}>{children}</article>
+      </MDXProvider>
     </Layout>
   );
 };
