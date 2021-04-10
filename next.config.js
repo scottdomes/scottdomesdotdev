4a0d6c39a7630613236aabb8b060f7d2833dac38
scottdomes <scottdomes@gmail.com>
2021-04-10 09:49:31 -0700

    Images working

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
+const copyLinkedFiles = require("remark-copy-linked-files");
+const path = require("path");
+const PATH_NAME = "articles";
+const destinationDir = path.join(__dirname, "public", PATH_NAME);
+
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [
+      [copyLinkedFiles, { destinationDir, staticPath: `/${PATH_NAME}` }],
+    ],
+  },
 });
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "mdx"],
