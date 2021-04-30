0402ff7167c528c502df632dc33ffd462f6ba196
scottdomes <scottdomes@gmail.com>
2021-04-30 09:17:13 -0700

    Node Display v1

--- components/articles/BinaryTrees/BinaryTree.js
+++ components/articles/BinaryTrees/BinaryTree.js
@@ -1,9 +1,51 @@
 import styles from "./BinaryTree.module.css";
 
+const tree = [1, null, 3, 2, 4, null, 5, 6];
+
+const treeAsObject = {
+  children: [],
+};
+
+const parents = [treeAsObject];
+let currentParentIndex = 0;
+tree.forEach((node) => {
+  if (node) {
+    const object = {
+      val: node,
+      children: [],
+    };
+    parents.push(object);
+    const currentParent = parents[currentParentIndex];
+    currentParent.children.push(object);
+  } else {
+    currentParentIndex++;
+  }
+});
+
+console.log(treeAsObject);
+
+const NodeDisplay = ({ node }) => {
+  return (
+    <li>
+      <div>
+        <span>{node.val}</span>
+      </div>
+      {node.children.length && (
+        <ul>
+          {node.children.map((child, key) => {
+            return <NodeDisplay node={child} key={child.val} />;
+          })}
+        </ul>
+      )}
+    </li>
+  );
+};
+
 const BinaryTree = () => {
   return (
     <div className={styles.tree}>
       <ul>
+        <NodeDisplay node={treeAsObject.children[0]} />
         <li>
           <div>
             <span>2</span>
