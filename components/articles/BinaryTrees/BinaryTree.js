40845daf1578951f7ebefbdefc3c36c413e60cbc
scottdomes <scottdomes@gmail.com>
2021-04-30 09:24:48 -0700

    Tracking visits v1

--- components/articles/BinaryTrees/BinaryTree.js
+++ components/articles/BinaryTrees/BinaryTree.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./BinaryTree.module.css";
 
 const tree = [1, null, 3, 2, 4, null, 5, 6];
@@ -22,18 +23,22 @@ tree.forEach((node) => {
   }
 });
 
-console.log(treeAsObject);
-
-const NodeDisplay = ({ node }) => {
+const NodeDisplay = ({ node, nodesByVal }) => {
   return (
     <li>
       <div>
         <span>{node.val}</span>
       </div>
-      {node.children.length && (
+      {node.children.length > 0 && (
         <ul>
           {node.children.map((child, key) => {
-            return <NodeDisplay node={child} key={child.val} />;
+            return (
+              <NodeDisplay
+                node={child}
+                key={child.val}
+                nodesByVal={nodesByVal}
+              />
+            );
           })}
         </ul>
       )}
@@ -42,39 +47,23 @@ const NodeDisplay = ({ node }) => {
 };
 
 const BinaryTree = () => {
+  const valueMapping = {};
+  tree.forEach((node) => {
+    if (node) {
+      valueMapping[node] = true;
+    }
+  });
+  const [nodesByVal, setNodesByVal] = useState(valueMapping);
+
+  const setVisited = (val, isVisited) => {
+    const copy = { ...nodesByVal, [val]: isVisited };
+    setNodesByVal(copy);
+  };
+
   return (
     <div className={styles.tree}>
       <ul>
-        <NodeDisplay node={treeAsObject.children[0]} />
-        <li>
-          <div>
-            <span>2</span>
-          </div>
-          <ul>
-            <li>
-              <div>
-                <span>1</span>
-              </div>
-            </li>
-            <li>
-              <div>
-                <span>4</span>
-              </div>
-              <ul>
-                <li>
-                  <div>
-                    <span>3</span>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <span>5</span>
-                  </div>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
+        <NodeDisplay node={treeAsObject.children[0]} nodesByVal={nodesByVal} />
       </ul>
     </div>
   );
